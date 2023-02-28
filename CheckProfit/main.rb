b06576938c71d222471be972d01bb3d3c2a1afe7
Shabanov-V <koprus77@gmail.com>
2023-02-28 16:04:21 +0200

    Add cloudwatch metric

--- CheckProfit/main.rb
+++ CheckProfit/main.rb
@@ -1,18 +1,37 @@
 require './PriceMonitor.rb'
 require './Notification.rb'
-
+require 'aws-sdk-cloudwatch'
 
 def handler(event:, context:)
-  
     topProfit = requestTopProfit()
     if (topProfit >= 50.0)
         sendNotification(topProfit)
     end
-
+    reportMetric(topProfit)
     {
       statusCode: 200,
       body: {
         topProfit: topProfit,
       }
     }
+end
+
+
+def reportMetric(metric) 
+  client = 	Aws::CloudWatch::Client.new
+  client.put_metric_data({
+    namespace: "SteamProfit",
+    metric_data: [{
+      metric_name: "Profit_by_percentages",
+      dimensions: [
+        {
+          name: "Percent", # required
+          value: "Time", # required
+        },
+      ],
+      timestamp: Time.now,
+      value: metric,
+      unit: "Percent"
+    }]
+  })
 end
--- main.rb
+++
@@ -1,8 +0,0 @@
-require './PriceMonitor.rb'
-require './Notification.rb'
-
-topProfit = requestTopProfit()
-
-if (topProfit >= 50.0)
-    sendNotification(topProfit)
-end
